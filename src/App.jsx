7f609acced89d871310b7c30211c85672c05f960
Vijay <vijayyadavp4082@gmai.com>
2026-09-03 20:03:47 +0530

    c1: added basic initial frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+  
+  return (
+   <div className="container py-5">
+
+      <div className="text-center">
+
+        <i className="bx bx-shopping-bag display-1 text-primary"></i>
+
+        <h1 className="fw-bold mt-3">
+          Amozone Ecommerce
+        </h1>
+
+        <p className="text-secondary">
+          Spring Boot + React Ecommerce Application
+        </p>
+
+        <button className="btn btn-primary px-4">
+          <i className="bx bx-shopping-bag me-2"></i>
+          Shop Now
+        </button>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
